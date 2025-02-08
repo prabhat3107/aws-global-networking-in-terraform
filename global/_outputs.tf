5760d39d7c42338e39d6975b01cad620664dfc22
Prabhat R Pradhan <prabhat3107@gmail.com>
2025-02-08 14:23:53 -0500

    updated s3 bucket name

--- global/_outputs.tf
+++ global/_outputs.tf
@@ -1,4 +1,4 @@
-output "brightkey_vpn" {
-  value       = var.brightkey_vpn
-  description = "Brightkey OpenVPN Endpoints"
+output "sriauronet_vpn" {
+  value       = var.sriauronet_vpn
+  description = "sriauronet OpenVPN Endpoints"
 }
--- global/_variables.tf
+++ global/_variables.tf
@@ -1,4 +1,4 @@
-variable "brightkey_vpn" {
-  description = "Brightkey OpenVPN Endpoints"
+variable "sriauronet_vpn" {
+  description = "sriauronet OpenVPN Endpoints"
   default     = ["1.2.3.4/32", "5.6.7.8/32"]
 }
